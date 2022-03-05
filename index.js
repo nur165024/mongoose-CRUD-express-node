3fabc8baf0c56a1fd26af7a78d4765707a42cd7d
nur165024 <nur42658@gmail.com>
2022-03-05 20:03:15 +0700

    first commit and mongoose and mongodb connection done

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// express app initialization
+const app = express();
+app.use(express.json());
+
+// database connect with mongoose
+mongoose
+  .connect('mongodb://localhost/todos')
+  .then(() => console.log('connection successfully!'))
+  .catch((err) => console.log(err));
+
+// app application routes
+
+// default error handler
+function errorHandler(err, req, res, next) {
+  if (res.headersSend) {
+    return next(err);
+  }
+  res.status(500).json({ error: err });
+}
+
+// app listening
+app.listen(5000, () => {
+  console.log(`app listening at post 5000`);
+});
